616ffe99b92ddcfa90dd3566a650871cc47d9bcb
Fabian <fabiancvm7@gmail.com>
2022-02-04 11:22:54 -0400

    [IMP] file env for mongo container

--- services/dollarRateService.js
+++ services/dollarRateService.js
@@ -1,48 +1,53 @@
-"use strict";
+'use strict';
 
-const CronJob = require("cron").CronJob;
-const axios = require("axios");
-const fs = require("fs");
-const moment = require("moment-timezone");
-const globalVar = require("../utils/serverCreation");
-const dollarRate = require("../files/dollarRate.json");
+const CronJob = require('cron').CronJob;
+const axios = require('axios');
+const fs = require('fs');
+const moment = require('moment-timezone');
+const globalVar = require('../utils/serverCreation');
+const dollarRate = require('../files/dollarRate.json');
 
 // Metodo para actualizar la tasa del dollar del día a través de dollar today
 const dollarRateCron = new CronJob(
-  "* * */1 * * *",
-  async () => {
-    const today = moment().format("YYYY-MM-DD hh:mm:ss A");
-    const responseFromDollarToday = await axios.get(globalVar.dollarRateUrl);
+	'* * */1 * * *',
+	async () => {
+		try {
+			const today = moment().format('YYYY-MM-DD hh:mm:ss A');
+			const responseFromDollarToday = await axios.get(globalVar.dollarRateUrl);
 
-    if (
-      dollarRate &&
-      responseFromDollarToday.data.USD.dolartoday !== dollarRate.usd.dollarToday
-    ) {
-      const dollarJson = JSON.stringify({
-        usd: {
-          dollarToday: responseFromDollarToday.data.USD.dolartoday,
-        },
-      });
+			if (
+				dollarRate &&
+				responseFromDollarToday.data.USD.dolartoday !==
+					dollarRate.usd.dollarToday
+			) {
+				const dollarJson = JSON.stringify({
+					usd: {
+						dollarToday: responseFromDollarToday.data.USD.dolartoday
+					}
+				});
 
-      fs.writeFile("files/dollarRate.json", dollarJson, (err, result) => {
-        if (err) {
-          console.error("El dolar no pudo ser actualizado");
-        } else
-          console.info(
-            `Dolar actualizado: ${responseFromDollarToday.data.USD.dolartoday} - ${today}`
-          );
-      });
-    }
-  },
-  null,
-  true,
-  "America/Caracas"
+				fs.writeFile('files/dollarRate.json', dollarJson, (err, result) => {
+					if (err) {
+						console.error('El dolar no pudo ser actualizado');
+					} else
+						console.info(
+							`Dolar actualizado: ${responseFromDollarToday.data.USD.dolartoday} - ${today}`
+						);
+				});
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	},
+	null,
+	true,
+	'America/Caracas'
 );
 
 const executeDollarRateCron = () => {
-  dollarRateCron.start();
+	dollarRateCron.start();
 };
 
 module.exports = {
-  executeDollarRateCron,
+	executeDollarRateCron
 };
